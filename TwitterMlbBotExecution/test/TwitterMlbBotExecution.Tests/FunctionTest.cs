c1ad1367b24a36d1f2ca7c16a5605fdd63b791fb
shin4488 <efshinya48@gmail.com>
2023-06-14 00:11:30 +0900

    change: directory structure the solution file is placed to the root

--- TwitterMlbBotExecution/test/TwitterMlbBotExecution.Tests/FunctionTest.cs
+++ TwitterMlbBotExecution/test/TwitterMlbBotExecution.Tests/FunctionTest.cs
@@ -12,6 +12,6 @@ public class FunctionTest
         // Invoke the lambda function and confirm the string was upper cased.
         var function = new Function();
         var context = new TestLambdaContext();
-        function.FunctionHandler("hello world", context);
+        function.FunctionHandlerAsync("hello world", context);
     }
 }
