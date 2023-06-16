405ae65cc3f6964d37ba2c41738b3efa4a42c332
shin4488 <efshinya48@gmail.com>
2023-06-16 22:18:02 +0900

    change: remove under score from member name

--- TwitterMlbBot/Mlb/MlbService.cs
+++ TwitterMlbBot/Mlb/MlbService.cs
@@ -8,19 +8,16 @@ namespace TwitterMlbBot.Mlb
 {
     class MlbService
     {
-        /// <summary>
-        /// WebAPIコールアウト用
-        /// </summary>
-        private static readonly HttpClient _client = new HttpClient();
-        private readonly string _endpoint = "https://api.sportsdata.io/v3/mlb/scores/json/Standings/";
-        private readonly string _apiKey;
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly string endpoint = "https://api.sportsdata.io/v3/mlb/scores/json/Standings/";
+        private readonly string apiKey;
 
         public MlbService()
         {
             // WebAPI認証用データ取得
             Dictionary<string, string> apiKeyConfig = ProcessUtility.ReadAppConfig("mlb");
             // AWSのlambda関数使用時はApp.configの値がnullとなるためnullチェックを入れる
-            _apiKey = ProcessUtility.GetEnvVarByKey(apiKeyConfig, "apiKey", "MLB_API_KEY");
+            this.apiKey = ProcessUtility.GetEnvVarByKey(apiKeyConfig, "apiKey", "MLB_API_KEY");
         }
 
         /// <summary>
@@ -29,15 +26,15 @@ namespace TwitterMlbBot.Mlb
         /// <returns>リーグごと・地区ごとのチームデータ</returns>
         public async Task<Result> GetStandingData(Param param)
         {
-            string uri = _endpoint + param.Year;
+            string uri = endpoint + param.Year;
             Dictionary<string, string> headers = new Dictionary<string, string>
             {
-                { "Ocp-Apim-Subscription-Key", _apiKey }
+                { "Ocp-Apim-Subscription-Key", this.apiKey }
             };
 
             // WebAPIコールアウト
             HttpResponseMessage response =
-                await ProcessUtility.CalloutAsync(_client, "GET", uri, headers, null);
+                await ProcessUtility.CalloutAsync(client, "GET", uri, headers, null);
             if (!response.IsSuccessStatusCode)
             {
                 // WebAPIレスポンスが200台:OK以外の場合は例外発生
